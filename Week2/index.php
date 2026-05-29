46f46a26321310b897be4420c1f016e329b55a0f
Joy3Nafula <nafulajoy505@gmail.com>
2026-05-29 07:31:49 +0300

    Delete Week2 directory

--- Week2/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// C:\xampp\htdocs\laptop_store\index.php
-
-echo "<h1>Laptop E-Commerce Store: Local Environment Setup Success</h1>";
-echo "<p>PHP Version: " . phpversion() . "</p>";
-echo "<p>System Status: Online and executing server-side scripts successfully.</p>";
-?>
